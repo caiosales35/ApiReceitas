b849afd910163c64c361f5ae78b2f0d079a1c297
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2021-01-16 01:34:04 -0300

    Recipe controller - rota para a criacao de receitas

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,8 +2,22 @@ class Api::V1::RecipesController < Api::V1::ApiController
 
     def index
         @recipes = Recipe.order(created_at: :desc)
-
         render json: @recipes
     end
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+        if @recipe.save
+            render json: @recipe, status: :created
+        else
+            render json: @recipe.errors, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+        def recipe_params
+            params.require(:recipe).permit(:title, :preparation_time, :oven_time, :ingredients, :steps, :Image, :category)
+        end
 
 end
